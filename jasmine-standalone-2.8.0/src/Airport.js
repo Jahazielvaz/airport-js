09ed4664857201f9401d3259daf543db5a0975db
Jahaziel <jahaziel.vaz@gmail.com>
2017-12-06 21:14:58 -0700

    Clear my first airport test

---
+++ jasmine-standalone-2.8.0/src/Airport.js
@@ -0,0 +1,14 @@
+var Airport = function(){
+  this.planes = function(){
+    return [];
+  };
+  this.clearForLanding = function(plane){
+    if(this.planes() = []){
+      return clearForLanding(plane);
+    }
+  }
+}
+
+var airport = new Airport();
+
+airport.clearForLanding("plane")
